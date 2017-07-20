092f39e4e6e5fdfdd7d96004c27cc69e676e6933
Matthew Drumheller <MattDrummy@gmail.com>
2017-07-20 14:10:36 -0600

    Second and Third Tests Passed

--- api/stickers.js
+++ api/stickers.js
@@ -7,16 +7,16 @@ router.get('/', (req, res) => {
   queries.getAll(res);
 });
 router.get('/:id', (req, res) => {
-  res.json(queries.getOne(req.params.id, res));
+  queries.getOne(req.params.id, res);
 });
 router.post('/', (req, res) => {
-  res.json(queries.create(req.body, res));
+  queries.create(req.body, res);
 });
 router.put('/:id', (req, res) => {
-  res.send(queries.update(req.params.id, req.body, res));
+  res.send(queries.update(req.params.id, req.body));
 });
 router.delete('/:id', (req, res) => {
-  res.send(queries.delete(req.params.id, res));
+  res.send(queries.delete(req.params.id));
 });
 
 module.exports = router;
--- db/queries.js
+++ db/queries.js
@@ -2,11 +2,14 @@ const knex = require('./knex'); // the connection!
 
 module.exports = {
   getAll(res) {
-    knex('sticker').select('*').then(function(data) {
+    knex('sticker').select('*').then((data) => {
       res.json(data);
     })
   },
   getOne(id, res) {
+    knex('sticker').select('*').where('id', id).first().then((data) => {
+      res.json(data);
+    })
   },
   create(sticker, res) {
   },
